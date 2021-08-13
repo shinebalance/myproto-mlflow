00bc69d2c034ce59c5d7cc572b637ef6dc262e8a
shinebalance <manazasi2020@outlook.com>
2021-08-13 19:23:00 +0900

    使用しない関数を削除

--- mlproject/train_and_predict.py
+++ mlproject/train_and_predict.py
@@ -87,14 +87,6 @@ def divide_xydatas(df_preprocessed:pd.DataFrame) -> Tuple[pd.DataFrame,pd.DataFr
     return train_x, test_x ,train_y, test_y
 
 
-def fetch_logged_data(run_id):
-    client = mlflow.tracking.MlflowClient()
-    data = client.get_run(run_id).data
-    tags = {k: v for k, v in data.tags.items() if not k.startswith("mlflow.")}
-    artifacts = [f.path for f in client.list_artifacts(run_id, "model")]
-    return data.params, data.metrics, tags, artifacts
-
-
 def rfc_with_mlflow(train_x, test_x ,train_y, test_y):
     random_forest = RandomForestClassifier(max_depth=5, n_estimators=10, random_state=42)
 
